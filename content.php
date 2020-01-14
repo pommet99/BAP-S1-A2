fe81bf14e6c062437faea127e5bd3fbddd53e828
flotr18 <55090943+flotr18@users.noreply.github.com>
2020-01-14 17:21:02 +0100

    Based Config

--- content.php
+++ content.php
@@ -1,14 +1,15 @@
-    <body>
+<?php die('ici'); ?>
+sdkhbvkhdsbvjsdvbjksvbksbvjksvbksdjvbdkvjbsdk
 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <img src="photos/baby.jpg" class="d-block w-100" alt="image enfant-maman">
+            <img src="<?php echo get_template_directory(); ?>/photos/baby.jpg" class="d-block w-100" alt="image enfant-maman">
         </div>
         <div class="carousel-item">
-            <img src="photos/baby2.jpg" class="d-block w-100" alt="image enfant-maman">
+            <img src="<?php echo get_template_directory_uri(); ?>/photos/baby2.jpg" class="d-block w-100" alt="image enfant-maman">
         </div>
         <div class="carousel-item">
-            <img src="photos/baby3.jpg" class="d-block w-100" alt="image enfant-maman">
+            <img src="<?php echo get_template_directory_uri(); ?>/photos/baby3.jpg" class="d-block w-100" alt="image enfant-maman">
         </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -20,7 +21,7 @@
         <span class="sr-only">Next</span>
     </a>
 </div>
-</body>
+
 <br><br><br>
 
 <!-- Body  -->
